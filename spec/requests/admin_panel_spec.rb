c5655a589952f992dfdf288e871839139125c9a9
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-04-23 18:30:16 +0200

    Extend specs for the admin panel.

--- spec/requests/admin_panel_spec.rb
+++ spec/requests/admin_panel_spec.rb
@@ -5,18 +5,14 @@ feature 'Admin panel' do
   describe 'an admin user' do
     let!(:user) { create(:user, :email => 'admin@email.com', :admin => true) }
 
-    scenario 'should be able to access the admin panel' do
+    background do
+      login_with(user)
       visit admin_dashboard_path
+    end
 
-      current_path.should == new_user_session_path
-      page.should have_content('You need to sign in or sign up before continuing.')
-
-      fill_in 'Email', :with => 'admin@email.com'
-      fill_in 'Password', :with => 'password'
-      click_button 'Sign in'
-
+    scenario 'should be able to access the admin panel' do
       current_path.should == admin_dashboard_path
-      page.should have_content('Signed in successfully.')
+      page.should have_content('Dashboard')
     end
   end
 
@@ -31,6 +27,14 @@ feature 'Admin panel' do
     scenario 'should not be able to access the admin panel' do
       current_path.should == new_user_session_path
       page.should have_content('You need to sign in or sign up before continuing.')
+    end
+
+    scenario 'should see the login form' do
+      current_path.should == new_user_session_path
+      page.should have_content('You need to sign in or sign up before continuing.')
+
+      fill_in 'Email', :with => 'admin@email.com'
+      fill_in 'Password', :with => 'password'
     end
   end
 
