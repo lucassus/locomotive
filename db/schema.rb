7168c9d85fe0abfb4fe5d71102f0142267921197
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-04-23 17:21:12 +0200

    #217297 Add User#admin flag.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120421132045) do
+ActiveRecord::Schema.define(:version => 20120423151848) do
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -24,10 +24,12 @@ ActiveRecord::Schema.define(:version => 20120421132045) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
   end
 
+  add_index "users", ["admin"], :name => "index_users_on_admin"
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
+# Create admin user
+admin_user = User.new do |u|
+  u.email = 'admin@email.com'
+  u.password = u.password_confirmation = 'password'
+  u.admin = true
+end
+admin_user.save!
+
 # Create first user account
 user = User.new do |u|
-  u.email 'user@email.com'
+  u.email = 'user@email.com'
   u.password = u.password_confirmation = 'password'
 end
 user.save!
