28575e0348630764433f6e6c396c52b20847a5a7
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-05-24 12:10:07 +0200

    Create sample application.yml config file.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   ## Twitter integration
-  provider :twitter, Settings.twitter_app_id, Settings.twitter_app_secret
+  twitter_config = Settings.omniauth.twitter
+  provider :twitter, twitter_config.app_id, twitter_config.app_secret
 
   ## Facebook integration
-  provider :facebook, Settings.facebook_app_id, Settings.facebook_app_secret,
+  facebook_config = Settings.omniauth.facebook
+  provider :facebook, facebook_config.app_id, facebook_config.app_secret,
     :scope =>  'publish_stream,offline_access',
     :client_options => { :ssl => { :ca_path => '/etc/ssl/certs'} }
 end
