9cd7dfd365469fc70fc367f29705a56df9730f6f
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-02-28 13:48:16 +0100

    Remove basic http auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_with_http_basic
   before_filter :set_locale
 
   private
@@ -25,16 +24,5 @@ class ApplicationController < ActionController::Base
   def default_url_options(options = {})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { locale: I18n.default_locale != I18n.locale ? I18n.locale : nil }
-  end
-
-  def authenticate_with_http_basic
-    return unless Rails.env.staging?
-
-    authenticate_or_request_with_http_basic do |given_login, given_password|
-      login = Settings.http_auth.login
-      password = Settings.http_auth.password
-
-      given_login == login && given_password == password
-    end
   end
 end
