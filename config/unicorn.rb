31d805e88934a9daebc4a9b98ddd853f3281db2b
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-03-08 14:20:24 +0100

    Add minimal unicorn configuration.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 working_directory "/home/locomotive/current"
-pid "/home/locomotive/current/tmp/pids/unicorn.pid"
-stderr_path "/home/locomotive/current/shared/log/unicorn.log"
-stdout_path "/home/locomotive/current/shared/log/unicorn.log"
+pid "/home/locomotive/shared/pids/unicorn.pid"
+stderr_path "/home/locomotive/shared/log/unicorn.log"
+stdout_path "/home/locomotive/shared/log/unicorn.log"
 
-listen "/home/locomotive/current/tmp/unicorn.sock"
+listen "/home/locomotive/shared/unicorn.sock"
 worker_processes 2
 timeout 30
